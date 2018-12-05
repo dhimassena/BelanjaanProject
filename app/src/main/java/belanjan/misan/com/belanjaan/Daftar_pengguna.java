ace731831798c3446bd917b5a4453f8f661e0539
dhimassena <dhimassr@gmail.com>
2018-12-05 16:54:02 +0700

    sena

---
+++ app/src/main/java/belanjan/misan/com/belanjaan/Daftar_pengguna.java
@@ -0,0 +1,29 @@
+package belanjan.misan.com.belanjaan;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Daftar_pengguna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_pengguna);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/belanjan/misan/com/belanjaan/MainActivity.java
+++ app/src/main/java/belanjan/misan/com/belanjaan/MainActivity.java
@@ -1,5 +1,6 @@
 package belanjan.misan.com.belanjaan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -87,7 +88,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
+        Intent daftar = new Intent(MainActivity.this, Daftar_pengguna.class);
+        startActivity(daftar);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
